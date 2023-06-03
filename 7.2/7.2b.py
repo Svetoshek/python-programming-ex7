85735df94070968c8ab65079627d8844d408ba00
Svetoshek <113710532+Svetoshek@users.noreply.github.com>
2023-06-03 21:08:40 +0300

    Update 7.2b.py

--- 7.2/7.2b.py
+++ 7.2/7.2b.py
@@ -1,12 +1,11 @@
 ignore = ["duplex", "alias", "configuration"]
-from sys import argv
-with open(argv[1]) as f, open(argv[2],"w") as output_file:
-    for string in f:
-        if string[0] != "!":
-            continue
-        contain = False
-        for i in ignore:
-            if i in string:
-                contain = True
-        if not contain:
-            output_file.write(string)
+
+src_file, dst_file = "config_sw1.txt", "config_sw1_cleared.txt"
+
+with open(src_file) as src, open(dst_file, 'w') as dst:
+    for line in src:
+        for ignore_word in ignore:
+            if line.startswith("!") or ignore_word in line:
+                break
+        else:
+            dst.write(line)
